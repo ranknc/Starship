dc6a1ddf1b060a15bae30f5f0b4995dc12c94260
Nick Rank <ranknc@s.dcsdk12.org>
2015-10-07 10:18:38 -0600

    Create player

---
+++ player.rb
@@ -0,0 +1,47 @@
+require "gosu"
+
+class Player
+
+	TURN_INCREMENT = 4.5
+	ACCELERATION = 0.5
+
+	def initialize
+		@x = @y = @vel_x = @vel_y = @angle = 0.0
+		@score = 0
+		@image = Gosu::Image.new("media/starfighter.bmp")
+	end
+
+	def warp(x, y)
+		@x, @y = x, y
+	end
+
+	def turn_left
+		@angle -= TURN_INCREMENT
+	end
+
+	def turn_right
+		@angle += TURN_INCREMENT
+	end
+
+	def accelerate
+		@vel_x += Gosu::offset_x(@angle, ACCELERATION)
+		@vel_y += Gosu::offset_y(@amgle, ACCELERATION)
+		
+	end
+
+	def move
+		@x += @vel_x
+		@y += @vel_y
+
+		@x %= 640
+		@y %= 480	
+
+		@vel_x *= 0.95
+		@vel_y *= 0.95
+	end
+
+	def draw
+		@image.draw.rot(@x, @y, 1, @angle)
+	end
+
+end
